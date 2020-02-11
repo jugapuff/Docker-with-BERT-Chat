05a8a14752f3212fb692a0a635e11f3f42f685bf
jugapuff <55308773+jugapuff@users.noreply.github.com>
2020-02-11 15:00:33 +0900

    initialize docker setting

---
+++ Docker_BERT_Chat/deploy/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+
+cd ../
+zip -r ./deploy/ceslea.zip ./pip.conf ./requirements.txt  ./src
+cd ./deploy
+
+docker stop kor_scenario_container
+docker rm kor_scenario_container
+docker build --network host -t kor_scenario_image .
+docker run -it --gpus all -p 40007:50051 --name kor_scenario_container kor_scenario_image
+
+rm -rf ./ceslea.zip
